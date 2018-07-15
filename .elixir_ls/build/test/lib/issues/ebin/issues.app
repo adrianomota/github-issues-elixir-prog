2c031c73cef94a16f2000002cfe89e01f895ee63
Adriano Mota <adrianomota@adriano-2.local>
2018-07-15 20:50:28 -0300

    adicionado escript

---
+++ .elixir_ls/build/test/lib/issues/ebin/issues.app
@@ -0,0 +1,10 @@
+{application,issues,
+             [{applications,[kernel,stdlib,elixir,logger,poison,httpoison]},
+              {description,"issues"},
+              {modules,['Elixir.Issues','Elixir.Issues.CLI',
+                        'Elixir.Issues.GithubIssues',
+                        'Elixir.Issues.TableFormatter',
+                        'Elixir.Mix.Tasks.Start']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {extra_applications,[logger]}]}.
--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -3,8 +3,10 @@ defmodule Issues.CLI do
   @default_count 4
 
   import IO, only: [puts: 1]
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
-  def run(argv) do
+
+  def main(argv) do
      argv
      |> parse_args
      |> process
@@ -36,7 +38,30 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn il, i2 -> il["created_at"] >= i2["created_at"] end)
+  end
+
+  def decode_response({:ok, body}) do
+      body
+  end
+
+  def decode_response({:error, error}) do
+    puts "Error fetching from Github: #{error["message"]}"
   end
 end
